4612d5d2adb7280ddf760552d822b63836576987
ZainyShorts <ZainyShorts@gmail.com>
2025-01-07 08:26:24 +0500

    bug fixes

--- src/app/(root)/blog/[blogId]/page.tsx
+++ src/app/(root)/blog/[blogId]/page.tsx
@@ -2,8 +2,8 @@ import Footer from "@/app/components/Footer/Footer";
 import Navbar from "@/app/components/Navbar/Navbar";
 
 import BlogPost from "./components/BlogPost";
-
-export default function page({ params }: { params: { blogId: string | number } }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function page({ params }: { params: { blogId: any } }) {
   console.log(params)
   return (
     <>
