737a5b5676ca8ef08b557bb7ad0e53ae01352a40
ZainyShorts <ZainyShorts@gmail.com>
2025-01-07 08:28:17 +0500

    bug fixes

--- src/app/(root)/blog/components/BlogCards/BlogData.ts
+++ src/app/(root)/blog/components/BlogCards/BlogData.ts
@@ -8,7 +8,7 @@ export const articles: Article[] = [
     description: "Sintra Mobile App: Take Control of Your Productivity Wherever You Are",
     date: "January 5, 2025",
     image: "https://cdn.prod.website-files.com/66912739c6023a7f5b275a1b/677a501883439ccc58b35d79_Sintra%20AI%20Goes%20Mobile_%20Introducing%20the%20iOS%20App%20%E2%80%93%20Manage%20Your%20AI%20Workforce%20Anytime%2C%20Anywhere.png",
-    href: "/blog/sintra-mobile"
+    href: "/blog"
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const articles: Article[] = [
     description: "Automating your marketing means using tools to handle everyday tasks. With AI-driven marketing automation, you can focus more on building relationships while the software handles routine tasks. Sintra makes this process even easier.",
     date: "November 18, 2024",
     image: "https://cdn.prod.website-files.com/66912739c6023a7f5b275a1b/673b5119268a96106a3e391d_5%20ways%20to%20Automate%20Your%20marketing%20Processes...-p-1080.png",
-    href: "/blog/marketing-automation"
+    href: "/blog"
   },
   {
     id: 3,
@@ -26,7 +26,7 @@ export const articles: Article[] = [
     description: "Having trouble managing customers' expectations? Well, you're not alone. The niches have become so competitive that it's hard to manage customers' expectations and the quality of the product. Good products and services are essential, but what really matters is how customers connect with your brand.",
     date: "November 18, 2024",
     image: "https://cdn.prod.website-files.com/66912739c6023a7f5b275a1b/673b510a6d4da1406809f65b_5%20Proven%20Ways%20to%20increase%20Customer%20Engagement-p-1080.png",
-    href: "/blog/customer-engagement"
+    href: "/blog"
   }
 ]
 
