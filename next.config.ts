92d3ecd925ecf8bd333611d7204d01d733ea9dc3
ZainyShorts <ZainyShorts@gmail.com>
2025-01-25 07:42:37 +0500

    eslint disable

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['cdn.prod.website-files.com' , 'lh3.googleusercontent.com','img.freepik.com','images.pexels.com'],
   },
